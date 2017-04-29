f4969e0a98da2a4ad5df96757b4ad88fe18cbb9e
Nick Mandrik <zvermandr@gmail.com>
2017-04-29 06:57:15 +0300

    test 2

--- app/src/test/java/com/yandex/mandrik/launcher/ExampleUnitTest.java
+++ app/src/test/java/com/yandex/mandrik/launcher/ExampleUnitTest.java
@@ -3,7 +3,6 @@ package com.yandex.mandrik.launcher;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
